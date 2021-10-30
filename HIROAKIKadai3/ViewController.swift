c72c2505a172a43b602e3440155ff6992d704632
akio0911 <akio0911@gmail.com>
2021-10-30 17:43:42 +0900

    feedback

--- HIROAKIKadai3/ViewController.swift
+++ HIROAKIKadai3/ViewController.swift
@@ -8,55 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet private weak var num1TextField: UITextField!
     @IBOutlet private weak var num2TextField: UITextField!
     @IBOutlet private weak var num1Label: UILabel!
     @IBOutlet private weak var num2Label: UILabel!
     @IBOutlet private weak var totalLabel: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        UserDefaults.standard.set(true, forKey: "num1Symbol")
-        UserDefaults.standard.set(true, forKey: "num2Symbol")
-    }
-
-    @IBAction private func num1SymbolSwitch(_ sender: UISwitch) {
-        if sender.isOn == true {
-            UserDefaults.standard.set(true, forKey: "num1Symbol")
-        } else {
-            UserDefaults.standard.set(false, forKey: "num1Symbol")
-        }
-    }
-
-    @IBAction private func num2SymbolSwitch(_ sender: UISwitch) {
-        if sender.isOn == true {
-            UserDefaults.standard.set(true, forKey: "num2Symbol")
-        } else {
-            UserDefaults.standard.set(false, forKey: "num2Symbol")
-        }
-    }
+    @IBOutlet private weak var sign1Switch: UISwitch!
+    @IBOutlet private weak var sign2Switch: UISwitch!
 
     @IBAction private func calcButton(_ sender: Any) {
-        var num1 = Int(num1TextField.text ?? "") ?? 0
-        var num2 = Int(num2TextField.text ?? "") ?? 0
+        let num1 = Int(num1TextField.text ?? "") ?? 0
+        let num2 = Int(num2TextField.text ?? "") ?? 0
 
-        let num1Symbol = UserDefaults.standard.bool(forKey: "num1Symbol")
-        let num2Symbol = UserDefaults.standard.bool(forKey: "num2Symbol")
-
-        if num1Symbol == true {
-            num1 = -num1
-            num1Label.text = String(num1)
+        let signedNum1: Int
+        if sign1Switch.isOn {
+            signedNum1 = -num1
         } else {
-            num1Label.text = String(num1)
+            signedNum1 = num1
         }
 
-        if num2Symbol == true {
-            num2 = -num2
-            num2Label.text = String(num2)
+        let signedNum2: Int
+        if sign2Switch.isOn {
+            signedNum2 = -num2
         } else {
-            num2Label.text = String(num2)
+            signedNum2 = num2
         }
-        totalLabel.text = String(num1 + num2)
+
+        num1Label.text = String(signedNum1)
+        num2Label.text = String(signedNum2)
+        totalLabel.text = String(signedNum1 + signedNum2)
     }
 }
